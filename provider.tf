0e9697badb377833d5f6043ea14b184548664c8a
Lukej-git <lukejtz@gmail.com>
2025-03-01 08:34:00 +0800

    Update provider.tf
    -Abstracted invocation for version.
    -Changed region to match account.

--- provider.tf
+++ provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.82.2"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
-  region = "ap-southeast-1"
+  region = "us-east-1"
 }
